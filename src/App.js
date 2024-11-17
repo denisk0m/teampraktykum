3a56fed91aef8892dc01e08e587d96372d0a6464
Marka15 <mariana.huchko@gmail.com>
2024-11-17 20:23:15 +0200

    add Mariana component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
 import Header from "./components/Header";
-import Body from "./components/Body";
+import Body from "./components/Body"
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Body />
+          <Body imgSrc="./PXL_20240808_122628861.jpg" name={"Mariana Huchko"} position={"Web Analyst"} />
     </div>
   );
 }
